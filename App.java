cdde1009b67cb594882d387ede2abcaeb44b6274
haeuni00 <leejw6311@naver.com>
2024-04-30 16:15:04 +0900

    lever2 7.

--- App.java
+++ App.java
@@ -3,43 +3,69 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         /* Calculator 인스턴스 생성 */
-        Calculator  cal = new Calculator();
+        Calculator cal = new Calculator();
 
         Scanner sc = new Scanner(System.in);
 
         /* 반복문 시작 */
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요:");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요:");
-            int num2 = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
-            double result = cal.calculate(num1, num2, operator);
+            System.out.println("사칙연산을 원하면 cal 입력, 원의 넓이를 원하면 circle 입력");
+            String choice = sc.next();
 
-            System.out.println("결과: " + result);
-            //cal.resultList.add(result);
-            cal.setList(result);
+            if(choice.equals("cal")) {
+                System.out.print("첫 번째 숫자를 입력하세요:");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요:");
+                int num2 = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
+                double result = cal.calculate(num1, num2, operator);
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = sc.next();
-            if(remove.equals("remove")){
-                //cal.resultList.remove(0);
-                cal.removeResult();
-            }
+                System.out.println("결과: " + result);
+                //cal.resultList.add(result);
+                cal.setList(result);
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiry = sc.next();
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.next();
+                if (remove.equals("remove")) {
+                    //cal.resultList.remove(0);
+                    cal.removeResult();
+                }
 
-            if(inquiry.equals("inquiry")){
-                // 저장된 연산 결과 조회
-               cal.inquiryResults();
-                // 향상된 for문으로 연산결과 전부 출력
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.next();
+
+                if (inquiry.equals("inquiry")) {
+                    // 저장된 연산 결과 조회
+                    cal.inquiryResults();
+                    // 향상된 for문으로 연산결과 전부 출력
 //                for(double listData: cal.getList()){
 //                    System.out.println(listData);
 //                }
+                }
             }
+            if(choice.equals("circle")){
+                System.out.println("원의 반지름을 입력하세요.");
+                int radius = sc.nextInt();
+                double cresult = cal.calculateCircleArea(radius);
 
+                System.out.println("결과: " + cresult);
+                cal.setCircleList(cresult);
+
+                System.out.println("가장 먼저 저장된 원의 넓이를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.next();
+                if (remove.equals("remove")) {
+                    cal.removeCResult();
+                }
+
+                System.out.println("저장된 원의넓이를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.next();
+
+                if (inquiry.equals("inquiry")) {
+                    // 저장된 원의 넓이 모두 조회
+                    cal.inquiryCResults();
+                }
+            }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = sc.next();
             if(exit.equals("exit")){
--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
     /* 연산 결과를 저장하는 컬렉션 타입 필드를 외부에서 직접 접근 하지 못하도록 수정*/
+    /* 연산 결과를 저장하는 컬렉션 타입 필드가 생성자를 통해 초기화 되도록 변경 */
     private ArrayList<Double> resultList = new ArrayList<Double>();
+    //원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성
+    private ArrayList<Double> circleList = new ArrayList<>();
+    // 원의 넓이를 구할때 파이는 변하지 않기 때문에 상수로 선언
+    static final double PI =3.14;
 
     public double calculate(int num1, int num2, char operator) throws Exception {
         double result=0;
@@ -28,6 +33,34 @@ public class Calculator {
             }
         return result;
     }
+
+    /* 원의 넓이를 구하는 메서드 선언*/
+    public double calculateCircleArea(int radius) {
+        /* 원의 넓이 계산 구현 */
+        double cResult = radius*radius*PI;
+        return cResult;
+    }
+    /* 원의 넓이 저장 필드 Getter, Setter, 조회 메서드 구현 */
+    public ArrayList<Double> getCircleList(){
+        return circleList;
+    }
+    // 원 넓이 리스트에 저장
+    public void setCircleList(double cResult){
+        circleList.add(cResult);
+    }
+    // 처음에 들어온 원넓이값 삭제
+    public void removeCResult(){
+        circleList.remove(0);
+    }
+    // 저장된 원 넓이들 조회
+    public void inquiryCResults() {
+        for(double listData: getCircleList()){
+            System.out.println(listData);
+        }
+    }
+
+
+    /* 사칙연산 결과 저장 필드 */
     /* Getter 메서드 구현 */
     public ArrayList<Double> getList(){
         return resultList;
@@ -36,7 +69,7 @@ public class Calculator {
     public void setList(double result){
         this.resultList.add(result);
     }
-    // 3. 에서 미리 해놔서 메서드 이름만 4. 문제에 맞게 변경
+    // 사칙연산 중 가장 처음에 연산된 결과 삭제
     public void removeResult(){
         this.resultList.remove(0);
     }
