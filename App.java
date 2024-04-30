9461deaa6ab1da178756045fdcb930b87de94ef8
haeuni00 <leejw6311@naver.com>
2024-04-30 10:44:52 +0900

    lever2 2.

---
+++ App.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /* Calculator 인스턴스 생성 */
+        Calculator  cal = new Calculator();
+
+        Scanner sc = new Scanner(System.in);
+
+        /* 반복문 시작 */
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요:");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요:");
+            int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+            double result = cal.calculate(num1, num2, operator);
+
+            System.out.println("결과: " + result);
+            cal.resultList.add(result);
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String remove = sc.next();
+            if(remove.equals("remove")){
+                cal.resultList.remove(0);
+            }
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            String inquiry = sc.next();
+            // 향상된 for문으로 연산결과 전부 출력
+            if(inquiry.equals("inquiry")){
+                for(double listData: cal.resultList){
+                    System.out.println(listData);
+                }
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if(exit.equals("exit")){
+                break;
+            }
+            /* 반복문 종료 */
+        }
+    }
+}
--- Calculator.java
+++ Calculator.java
@@ -3,22 +3,22 @@ public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
     ArrayList<Double> resultList = new ArrayList<Double>();
 
-    public double calculate(int num1, int num2, String operator) throws Exception {
+    public double calculate(int num1, int num2, char operator) throws Exception {
         double result=0;
-            if(!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
-                throw new InputException();
+            if(operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                throw new OpInputException();
             }
             switch (operator) {
-                case "+":
+                case '+':
                     result = num1 + num2;
                     break;
-                case "-":
+                case '-':
                     result = num1 - num2;
                     break;
-                case "*":
+                case '*':
                     result = num1 * num2;
                     break;
-                case "/":
+                case '/':
                         if (num2 == 0) {
                             throw new InputException();
                         }
@@ -31,6 +31,11 @@ public class Calculator {
 
 class InputException extends Exception{
     public InputException(){
-        super("예외 발생");
+        super("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+    }
+}
+class OpInputException extends Exception{
+    public OpInputException(){
+        super("+, -, *, / 중에 입력해야합니다.");
     }
 }
