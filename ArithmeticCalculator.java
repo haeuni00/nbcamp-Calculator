c361cb48d7294f2a8e5d5a63d25b045e985224f3
haeuni00 <leejw6311@naver.com>
2024-05-01 11:24:26 +0900

    lever2 8.

--- ArithmeticCalculator.java
+++ ArithmeticCalculator.java
@@ -1,8 +1,6 @@
 public class ArithmeticCalculator extends Calculator{
     public double calculate(int num1, int num2, char operator) {
         double result=0;
-        if(operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-        }
         switch (operator) {
             case '+':
                 result = num1 + num2;
@@ -14,8 +12,6 @@ public class ArithmeticCalculator extends Calculator{
                 result = num1 * num2;
                 break;
             case '/':
-                if (num2 == 0) {
-                }
                 result = num1 / num2;
                 break;
         }
